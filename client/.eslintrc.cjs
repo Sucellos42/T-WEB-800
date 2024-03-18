376d33452ceeaeb02257de588248cf5f2bffa0c6
Léo Recouvreux <leo.recouvreux@gmail.com>
2024-03-18 16:05:11 +0100

    Fix eslint parser

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+};
