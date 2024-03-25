b9d73ca994b1d1c525bdad83187dd7df06372255
francois <langfrancoisfrederic@gmail.com>
2024-03-25 16:34:23 +0100

    WIP merge repo epitech into new repo

--- frontend/src/components/common/CardCommon.vue
+++ frontend/src/components/common/CardCommon.vue
@@ -1,49 +1,71 @@
 <script setup lang="ts">
-import { defineProps, ref, watch, defineEmits } from 'vue'
-import { useConnexionStore } from '~/stores/connexion/connexion.store.ts'
-import { DatePicker as VDatePicker } from "v-calendar";
+import { defineProps, ref, watch, defineEmits } from 'vue';
+import { useConnexionStore } from '~/stores/connexion/connexion.store.ts';
+import { DatePicker as VDatePicker } from 'v-calendar';
+import { RangeDateSelected } from '~/types/rangeDateSelected.type.ts';
 
 const props = defineProps<{
-  type: String;
+  type: string;
 }>();
 
-const selectedRange = ref({ start: null, end: null });
+const selectedRange = ref({
+  start: new Date(),
+  end: new Date(),
+} as RangeDateSelected);
 const attributes = ref([{}]);
 
-const connexionStore = useConnexionStore()
-const token: string | null = localStorage.getItem('token') ?? null
-const log: string = token ? 'Déconnexion' : 'Connexion'
+const connexionStore = useConnexionStore();
+const token: string | null = localStorage.getItem('token') ?? null;
+const log: string = token ? 'Déconnexion' : 'Connexion';
 
-
-watch(selectedRange, (newRange) => {
-  if (newRange.start && newRange.end) {
-    attributes.value = [
-      {
-        highlight: {
-          start: { fillMode: 'outline' },
-          base: { fillMode: 'light' },
-          end: { fillMode: 'outline' },
+watch(
+  selectedRange,
+  (newRange) => {
+    if (newRange.start && newRange.end) {
+      attributes.value = [
+        {
+          highlight: {
+            start: { fillMode: 'outline' },
+            base: { fillMode: 'light' },
+            end: { fillMode: 'outline' },
+          },
+          dates: { start: newRange.start, end: newRange.end },
         },
-        dates: { start: newRange.start, end: newRange.end },
-      },
-    ];
-    emit('update:selectedRange', newRange)
-  }
-}, { deep: true });
+      ];
+      emit('update:selectedRange', newRange);
+    }
+  },
+  { deep: true },
+);
 
-const emit = defineEmits(['update:selectedRange'])
+const emit = defineEmits(['update:selectedRange']);
 </script>
 
 <template>
-  <div v-if="props.type === 'connexion'" class="flex flex-col border-0.5 border-gray-300 rounded-md p-4 gap-y-0.5">
-    <span v-if="!token" @click="connexionStore.oAuthLogin()" class="hover:text-red-500">Inscription</span>
-    <span @click="connexionStore.logout()" class="hover:text-red-500">{{ log }}</span>
+  <div
+    v-if="props.type === 'connexion'"
+    class="flex flex-col border-0.5 border-gray-300 rounded-md p-4 gap-y-0.5"
+  >
+    <span
+      v-if="!token"
+      class="hover:text-red-500"
+      @click="connexionStore.oAuthLogin()"
+    >
+      Inscription
+    </span>
+    <span class="hover:text-red-500" @click="connexionStore.logout()">
+      {{ log }}
+    </span>
   </div>
-  <VDatePicker v-if="props.type === 'arrivee' || props.type === 'depart'" v-model.range="selectedRange" :attributes="attributes" :min-date="new Date()" expanded />
+  <VDatePicker
+    v-if="props.type === 'arrivee' || props.type === 'depart'"
+    v-model.range="selectedRange"
+    :attributes="attributes"
+    :min-date="new Date()"
+    model="dateTime"
+    expanded
+  />
   <div v-if="props.type === 'prix'"></div>
-
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
