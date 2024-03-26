bb6addd546f168c9bcd016bb411826f32179eec7
tommaitret <tom.maitret@gmail.com>
2024-03-26 11:37:35 +0100

    merge avec l'ancien back, setup microservices

--- back/authservice/src/app.controller.ts
+++ back/authservice/src/app.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Req, UseGuards } from '@nestjs/common';
+import {Controller, Req, Res, UseGuards} from '@nestjs/common';
 import { AppService } from './app.service';
 import { MessagePattern } from '@nestjs/microservices';
 import { AuthGuard } from '@nestjs/passport';
+import {OAuthInterface} from '../interfaces/oAuth.interface';
+import {Request, Response} from 'express'
 
 @Controller()
 export class AppController {
@@ -10,11 +12,17 @@ export class AppController {
   @MessagePattern({ cmd: 'get_google_auth' })
   @UseGuards(AuthGuard('google'))
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async googleAuth(@Req() req) {}
+  async googleAuth() {}
 
   @MessagePattern({ cmd: 'get_google_redirect' })
   @UseGuards(AuthGuard('google'))
-  googleAuthRedirect(@Req() req) {
-    return this.appService.googleLogin(req);
+  async googleAuthRedirect(@Req() req: Request, @Res() res: Response) {
+    const data: OAuthInterface = this.appService.googleLogin(req);
+    const token: string = encodeURIComponent(data.user.accessToken);
+    const firstName: string = encodeURIComponent(data.user.firstName);
+    const redirectUrl = `${process.env.URL_FRONTEND}?token=${token}&firstName=${firstName}`;
+    if (token) {
+      res.redirect(redirectUrl);
+    }
   }
 }
--- back/authservice/src/app.service.ts
+++ back/authservice/src/app.service.ts
@@ -1,15 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, UnauthorizedException} from '@nestjs/common';
+import {OAuthInterface} from "../interfaces/oAuth.interface";
+import {UserOAuthInterface} from "../interfaces/userOAuth.interface";
 
 @Injectable()
 export class AppService {
-  googleLogin(req) {
+  googleLogin(req): OAuthInterface {
     if (!req.user) {
-      return 'No user from google';
+      throw new UnauthorizedException('No user from google');
     }
+
+    const userOAuth: UserOAuthInterface = {
+      email: req.user.email,
+      firstName: req.user.firstName,
+      lastName: req.user.lastName,
+      picture: req.user.picture,
+      accessToken: req.user.accessToken,
+    };
 
     return {
       message: 'User information from google',
-      user: req.user,
+      user: userOAuth,
     };
   }
 }
