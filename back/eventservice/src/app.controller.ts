1ef3c6cbf62ef416caa5a762b5678d17b30b63c0
tommaitret <tom.maitret@gmail.com>
2024-04-15 15:08:26 +0200

    get event by coordinates/ by type and city

--- back/eventservice/src/app.controller.ts
+++ back/eventservice/src/app.controller.ts
@@ -11,4 +11,26 @@ export class AppController {
   async getEventsByCity(city:string): Promise<EventInterface[]> {
     return this.appService.getEventsByCity(city);
   }
+
+  @MessagePattern({ cmd: 'get_events_by_type' })
+  async getEventsByType(type:Array<string>): Promise<EventInterface[]> {
+    return this.appService.getEventsByType(type);
+  }
+
+  @MessagePattern({ cmd: 'get_events_by_coordinates' })
+  async getEventsByCoordinates(coordinates:Array<string>): Promise<EventInterface[]> {
+    return this.appService.getEventsByCoordinates(coordinates);
+  }
+
+    @MessagePattern({ cmd: 'get_events_by_city_and_type' })
+    async getEventsByCityAndType(city:string, type:Array<string>): Promise<EventInterface[]> {
+
+      let selectedCity = city[0];
+      let allType = [];
+        for (let i = 0; i < city[1].length; i++) {
+            allType.push(city[1][i]);
+        }
+      console.log(allType)
+      return this.appService.getEventsByCityAndType(selectedCity, allType);
+    }
 }
