388eb02147022d6c3ee73448c0a01d02c1e52730
francois <langfrancoisfrederic@gmail.com>
2024-04-16 17:02:48 +0200

    WIP refacto all types events

--- frontend/src/types/inputCommon/dataInput.type.ts
+++
@@ -1,9 +0,0 @@
-import { RangeDateSelected } from '../date/rangeDateSelected.type.ts';
-import { EventsSelected } from '../events/events.type.ts';
-
-export type DataInput = {
-  city: string;
-  date: RangeDateSelected;
-  events: EventsSelected;
-  eventsTranslated: EventsSelected;
-};
