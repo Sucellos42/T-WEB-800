96df89133a764b7ec72a515d816059645ef4d857
gschatz <gregory.schatz@epitech.eu>
2024-04-16 17:07:36 +0200

    Add call to get city

--- back/client/src/app.controller.ts
+++ back/client/src/app.controller.ts
@@ -1,30 +1,50 @@
-import {Controller, Get, Inject, Param} from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Controller, Get, Inject, Param } from "@nestjs/common";
+import { ClientProxy } from "@nestjs/microservices";
 
-@Controller('events')
+@Controller("events")
 export class AppController {
-  constructor(@Inject('EVENTS_SERVICE') private client: ClientProxy) {}
+  constructor(@Inject("EVENTS_SERVICE") private client: ClientProxy) {}
 
-  @Get('/bycity/:city')
-  getAllEvents(@Param('city') city: string){
-    return this.client.send({ cmd: 'get_events' } , city);
+  @Get("/bycity/:city")
+  getAllEvents(@Param("city") city: string) {
+    return this.client.send({ cmd: "get_events" }, city);
   }
-  @Get('/bytype/:type')
-  getAllEventsByType(@Param('type') type:string){
-    let typeArray = type.split(':');
+  @Get("/bytype/:type")
+  getAllEventsByType(@Param("type") type: string) {
+    let typeArray = type.split(":");
     console.log(typeArray);
-    return this.client.send({ cmd: 'get_events_by_type' } , typeArray);
+    return this.client.send({ cmd: "get_events_by_type" }, typeArray);
   }
 
-  @Get('/bycoordinates/:latitudea/:latitudeb/:longitudea/:longitudeb')
-    getAllEventsByCoordinates(@Param('latitudea') latitudea: string, @Param('latitudeb') latitudeb: string, @Param('longitudea') longitudea: string, @Param('longitudeb') longitudeb: string){
-        return this.client.send({ cmd: 'get_events_by_coordinates' } , [latitudea, latitudeb, longitudea, longitudeb]);
-    }
-    @Get('/bycityandtype/:city/:type')
-    getAllEventsByCityAndType(@Param('city') city: string, @Param('type') type:string){
-        let typeArray = type.split(':');
-        console.log(typeArray);
-        return this.client.send({ cmd: 'get_events_by_city_and_type' } , [city, typeArray]);
-    }
+  @Get("/bycoordinates/:latitudea/:latitudeb/:longitudea/:longitudeb")
+  getAllEventsByCoordinates(
+    @Param("latitudea") latitudea: string,
+    @Param("latitudeb") latitudeb: string,
+    @Param("longitudea") longitudea: string,
+    @Param("longitudeb") longitudeb: string
+  ) {
+    return this.client.send({ cmd: "get_events_by_coordinates" }, [
+      latitudea,
+      latitudeb,
+      longitudea,
+      longitudeb,
+    ]);
+  }
+  @Get("/bycityandtype/:city/:type")
+  getAllEventsByCityAndType(
+    @Param("city") city: string,
+    @Param("type") type: string
+  ) {
+    let typeArray = type.split(":");
+    console.log(typeArray);
+    return this.client.send({ cmd: "get_events_by_city_and_type" }, [
+      city,
+      typeArray,
+    ]);
+  }
 
+  @Get("/getcity")
+  getCity() {
+    return this.client.send({ cmd: "get_city" }, "");
+  }
 }
--- back/eventservice/src/app.controller.ts
+++ back/eventservice/src/app.controller.ts
@@ -1,36 +1,45 @@
-import { Controller } from '@nestjs/common';
-import { AppService } from './app.service';
-import { MessagePattern } from '@nestjs/microservices';
-import { EventInterface } from './interfaces/event.interface';
+import { Controller } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { MessagePattern } from "@nestjs/microservices";
+import { EventInterface } from "./interfaces/event.interface";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @MessagePattern({ cmd: 'get_events' })
-  async getEventsByCity(city:string): Promise<EventInterface[]> {
+  @MessagePattern({ cmd: "get_events" })
+  async getEventsByCity(city: string): Promise<EventInterface[]> {
     return this.appService.getEventsByCity(city);
   }
 
-  @MessagePattern({ cmd: 'get_events_by_type' })
-  async getEventsByType(type:Array<string>): Promise<EventInterface[]> {
+  @MessagePattern({ cmd: "get_events_by_type" })
+  async getEventsByType(type: Array<string>): Promise<EventInterface[]> {
     return this.appService.getEventsByType(type);
   }
 
-  @MessagePattern({ cmd: 'get_events_by_coordinates' })
-  async getEventsByCoordinates(coordinates:Array<string>): Promise<EventInterface[]> {
+  @MessagePattern({ cmd: "get_events_by_coordinates" })
+  async getEventsByCoordinates(
+    coordinates: Array<string>
+  ): Promise<EventInterface[]> {
     return this.appService.getEventsByCoordinates(coordinates);
   }
 
-    @MessagePattern({ cmd: 'get_events_by_city_and_type' })
-    async getEventsByCityAndType(city:string, type:Array<string>): Promise<EventInterface[]> {
-
-      let selectedCity = city[0];
-      let allType = [];
-        for (let i = 0; i < city[1].length; i++) {
-            allType.push(city[1][i]);
-        }
-      console.log(allType)
-      return this.appService.getEventsByCityAndType(selectedCity, allType);
+  @MessagePattern({ cmd: "get_events_by_city_and_type" })
+  async getEventsByCityAndType(
+    city: string,
+    type: Array<string>
+  ): Promise<EventInterface[]> {
+    let selectedCity = city[0];
+    let allType = [];
+    for (let i = 0; i < city[1].length; i++) {
+      allType.push(city[1][i]);
     }
+    console.log(allType);
+    return this.appService.getEventsByCityAndType(selectedCity, allType);
+  }
+
+  @MessagePattern({ cmd: "get_city" })
+  async getCity(): Promise<string[]> {
+    return this.appService.getCity();
+  }
 }
