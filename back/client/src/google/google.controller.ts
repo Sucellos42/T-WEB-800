9d36e38c0d203b6a21d22421b41426a5eeb32e4d
tommaitret <tom.maitret@gmail.com>
2024-04-23 16:50:57 +0200

    documentation des routes du controller google

--- back/client/src/google/google.controller.ts
+++ back/client/src/google/google.controller.ts
@@ -3,7 +3,7 @@ import { GoogleService} from "./google.service";
 import { AuthGuard } from '@nestjs/passport';
 import {OAuthInterface} from "../../interfaces/oAuth.interface";
 import { Request, Response } from 'express';
-import {ApiTags} from "@nestjs/swagger";
+import {ApiInternalServerErrorResponse, ApiOkResponse, ApiOperation, ApiTags} from "@nestjs/swagger";
 
 @ApiTags('OAuth with Google')
 @Controller('google')
@@ -12,10 +12,16 @@ export class GoogleController {
 
   @Get()
   @UseGuards(AuthGuard('google'))
+  @ApiOperation({ summary: 'This endpoint allows you to authenticate with Google.',
+    description: 'This endpoint allows you to authenticate with Google. It operates using the GET method. It redirects you to the Google authentication page.' })
+  @ApiOkResponse({ description: 'Google authentication successful. Redirect to home page' })
+  @ApiInternalServerErrorResponse({ description: 'Internal error.' })
   async googleAuth(@Req() req) {}
 
   @Get('redirect')
   @UseGuards(AuthGuard('google'))
+    @ApiOperation({ summary: '(Do not use this endpoint directly !) - Redirection endpoint after Google authentication.',
+        description: 'This endpoint is used to redirect you to the home page after Google authentication. It operates using the GET method. Do not use this endpoint directly.' })
   async googleAuthRedirect(@Req() req: Request, @Res() res: Response) {
     const data: OAuthInterface = this.googleService.googleLogin(req);
     const token: string = encodeURIComponent(data.user.accessToken);
