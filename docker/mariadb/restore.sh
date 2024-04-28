63483ba96735a10525ad19dc2f61eff415d122bb
Léo Recouvreux <leo.recouvreux@gmail.com>
2024-04-28 17:55:22 +0200

    setup mariadb container

---
+++ docker/mariadb/restore.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script pour restaurer la base de données à partir d'un dump SQL
+
+# Attendre que le serveur MariaDB soit prêt à accepter des commandes
+#until echo '\q' | mariadb -u root -p"$MYSQL_ROOT_PASSWORD" -h localhsot -p 3306; do
+#    >&2 echo "MariaDB is unavailable - sleeping"
+#    sleep 1
+#done
+>&2 echo "MariaDB is up - executing command"
+
+## Restaurer la base de données à partir du fichier dump.sql
+#mariadb -h 0.0.0.0 -P 3306 -u root -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" < /docker-entrypoint-initdb.d/dump.sql
+#mariadb -h 0.0.0.0 -P 3306 -u root -p
